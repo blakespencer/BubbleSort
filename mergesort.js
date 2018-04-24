51b83bcefe4e1587252b20eba0940be92a17ac42
Blake Spencer <blake.spencer1@gmail.com>
2018-04-23 21:50:24 -0400

    uploaded with Gabriel's solutions

--- mergesort.js
+++ mergesort.js
@@ -1,27 +1,47 @@
-function split(wholeArray) {
-  let half = Math.round((wholeArray.length - 1) / 2);
+function split (wholeArray) {
+  let half = wholeArray.length / 2;
   let firstHalf = wholeArray.slice(0, half);
-  let secondHalf = wholeArray.slice(half,wholeArray.length);
+  let secondHalf = wholeArray.slice(half);
   return [firstHalf, secondHalf];
 }
 
-function merge(nestedArr) {
-  const output = [];
-  let left = nestedArr[0];
-  let right = nestedArr[1];
-  while (nestedArr[0].length && nestedArr[1].length) {
-    if (left[0] < right[0]) {
-      output.push(left.shift());
+function merge(left, right) {
+  let output = [],
+      leftInx = 0,
+      rightInx = 0;
+  while (leftInx < left.length && rightInx < right.length) {
+    if (left[leftInx] < right[rightInx]) {
+      output.push(left[leftInx]);
+      leftInx++;
     } else {
-      output.push(right.shift());
+      output.push(right[rightInx]);
+      rightInx++
     }
-    left = nestedArr[0];
-    right = nestedArr[1];
   }
-  if (right) {
-    output.push(...right);
-  } else if (left) {
-    output.push(...left);
-  } return output;
+  for (;leftInx < left.length; leftInx++) output.push(left[leftInx]);
+  for (;rightInx < right.length; rightInx++) output.push(right[rightInx]);
+  return output;
 }
 
+
+function mergeSort(arr) {
+  // let output = split(arr);
+  // let left = output[0];
+  // let right = output[1];
+  // if (left.length !== 1 && right.length !== 1) {
+  //   left = mergeSort(left);
+  //   right = mergeSort(right);
+  // } else if (right.length !== 1) {
+  //   right = mergeSort(right);
+  // } else if (left.length !== 1){
+  //   left = mergeSort(left);
+  // }
+  // let final = merge([left, right]);
+
+  // return final;
+  if (arr.length < 2) {
+    return arr;
+  }
+  let splits = split(arr);
+  return merge(mergeSort(splits[0]), mergeSort(splits[1]));
+}
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -12,15 +12,25 @@ describe('Split Array function', function() {
 
 describe('Merge function', function() {
   it('is able to merge two sorted arrays into one sorted array', function() {
-    expect(merge([[1, 3], [2, 4]])).toEqual([1, 2, 3, 4]);
+    expect(merge([1, 3], [2, 4])).toEqual([1, 2, 3, 4]);
   });
   it('is able to merge two sorted arrays into one sorted array even different', function() {
-    expect(merge([[1, 2], [3, 4]])).toEqual([1, 2, 3, 4]);
+    expect(merge([1, 2], [3, 4])).toEqual([1, 2, 3, 4]);
   });
   it('is able to merge two sorted arrays into one sorted array odd good', function() {
-    expect(merge([[1, 2], [4, 5, 6]])).toEqual([1, 2, 4, 5, 6]);
+    expect(merge([1, 2], [4, 5, 6])).toEqual([1, 2, 4, 5, 6]);
   });
   it('is able to merge two sorted arrays into one sorted array, odd different', function() {
-    expect(merge([[1, 3], [2, 4, 6]])).toEqual([1, 2, 3, 4, 6]);
+    expect(merge([1, 3], [2, 4, 6])).toEqual([1, 2, 3, 4, 6]);
+  });
+});
+
+
+describe('mergeSort', function() {
+  it('while loop hack', function(){
+    expect(mergeSort([1, 8, 3, 5])).toEqual([1, 3, 5, 8]);
+  });
+  it('while loop hack odd', function(){
+    expect(mergeSort([1, 8, 3, 5, 4])).toEqual([1, 3, 4, 5, 8]);
   });
 });
